dceb88bbb88aaf886b19c9b49d970d0f805a30f9
DarrenN <info@v25media.com>
2017-09-07 15:36:29 -0400

    Refactors extract-filedata and adds tests

--- private/pdf.rkt
+++ private/pdf.rkt
@@ -21,8 +21,7 @@
       string-normalize-spaces))
 
 (define (extract-filedata repo-path path)
-  (call-with-input-file path #:mode 'binary
-    (λ (in)
+  (λ (in)
       (define-values
         (directory-path file-path is-dir)
         (split-path (find-relative-path repo-path path)))
@@ -32,7 +31,7 @@
            (path->string directory-path)
            (normalize-filename file-path)
            datetime
-           datetime))))
+           datetime)))
 
 (define (handle-pdfs state)
   (define REPO-PATH (expand-user-path
@@ -45,9 +44,11 @@
   ; will be #f is any PDFs cased a non-dupe SQLError
   (define saved-pdfs?
     (for/and ([pdf-path PDFS])
-      (equal?
-       'ok
-       (insert-file LOGGER SQLITE-CONN (extract-filedata REPO-PATH pdf-path)))))
+      (equal? 'ok
+              (insert-file
+               LOGGER SQLITE-CONN
+               (call-with-input-file pdf-path #:mode 'binary
+                 (extract-filedata REPO-PATH pdf-path))))))
 
   (if saved-pdfs?
       (append state '("PDFs processed"))
@@ -57,3 +58,34 @@
   (if (equal? (car state) 'error)
       state
       (handle-pdfs state)))
+
+(module+ test
+  (require rackunit)
+
+  (test-case "extract-filedata returns a pdf struct from an input-port"
+    (define re-date #px"^[\\d]{4}-[\\d]{2}-[\\d]{2}T[\\d]{2}:[\\d]{2}:[\\d]{2}.[\\d]*")
+    (define in (open-input-bytes #"(i got a letter from the government)"))
+    (define readfn (extract-filedata
+                    (string->path "papers-we-love/papers")
+                    (string->path "papers-we-love/papers/ai/HAL-2000.pdf")))
+    (define p (readfn in))
+    (check-equal? (pdf-sha1 p) "07eba1b33856e41e2f99e8aead30762a41da3ca3")
+    (check-equal? (pdf-filename p) "HAL-2000.pdf")
+    (check-equal? (pdf-directory p) "ai/")
+    (check-equal? (pdf-normalized p) "hal 2000")
+    (check-regexp-match re-date (pdf-created p))
+    (check-regexp-match re-date (pdf-modified p)))
+
+  (test-case "process-pdfs let's error state fall through"
+    (define err '(error ("Bad things went down")))
+    (check-equal? err (process-pdfs err)))
+
+  (test-case "normalize file name does what it says"
+    (check-equal? (normalize-filename (string->path "/sabrina/the/witch.exe"))
+                  "/sabrina/the/witch")
+    (check-equal? (normalize-filename (string->path "/Sabrina/tHe/WiTcH.exeE"))
+                  "/sabrina/the/witch")
+    (check-equal? (normalize-filename (string->path "Sabrina-the_witch.pdf"))
+                  "sabrina the witch")
+    (check-equal? (normalize-filename (string->path "   sabrina The   witch"))
+                  "sabrina the witch")))
